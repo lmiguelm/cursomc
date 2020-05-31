b09de773a4ef31249bb7693d504218e82f17f860
lmiguelm <luismiguelfernandes.marcelo@gmail.com>
2020-05-31 17:06:40 -0300

    ajustes no endpoint /categorias/{id}

--- src/main/java/com/lmiguel/cursomc/services/CategoriaService.java
+++ src/main/java/com/lmiguel/cursomc/services/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.lmiguel.cursomc.domain.Categoria;
 import com.lmiguel.cursomc.repositories.CategoriaRepository;
+import com.lmiguel.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -16,7 +17,7 @@ public class CategoriaService {
 	
 	public Categoria buscar(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
 	
 }
---
+++ src/main/java/com/lmiguel/cursomc/services/ProdutoService.java
@@ -0,0 +1,22 @@
+package com.lmiguel.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.lmiguel.cursomc.domain.Produto;
+import com.lmiguel.cursomc.repositories.ProdutoRepository;
+
+@Service
+public class ProdutoService {
+	
+	@Autowired
+	private ProdutoRepository repo;
+	
+	public Produto buscar(Integer id) {
+		Optional<Produto> obj = repo.findById(id);
+		return obj.orElse(null);
+	}
+	
+}
