d20344ef6ef1bb23c311dc46592d2fdbdac6930b
lmiguelm <luismiguelfernandes.marcelo@gmail.com>
2020-05-31 13:24:50 -0300

    testando o REST

---
+++ src/main/java/com/lmiguel/cursomc/domain/Categoria.java
@@ -0,0 +1,11 @@
+package com.lmiguel.cursomc.domain;
+
+import java.io.Serializable;
+
+public class Categoria implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	
+	
+
+}
