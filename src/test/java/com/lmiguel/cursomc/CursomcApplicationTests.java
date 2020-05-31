ebb872dd42cf0373df07b3d7e1d4279187e1a2ed
lmiguelm <luismiguelfernandes.marcelo@gmail.com>
2020-05-31 13:14:46 -0300

    Projeto criado

---
+++ src/test/java/com/lmiguel/cursomc/CursomcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lmiguel.cursomc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursomcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
