9e6e7d052ee13d590f797bd7f0620d4aeae4097d
lmiguelm <luismiguelfernandes.marcelo@gmail.com>
2020-06-01 21:24:02 -0300

    valicação sintatica com Bean Validation

--- src/main/java/com/lmiguel/cursomc/dto/CategoriaDTO.java
+++ src/main/java/com/lmiguel/cursomc/dto/CategoriaDTO.java
@@ -2,17 +2,22 @@ package com.lmiguel.cursomc.dto;
 
 import java.io.Serializable;
 
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
+
 import com.lmiguel.cursomc.domain.Categoria;
 
 public class CategoriaDTO implements Serializable {
-	
 	private static final long serialVersionUID = 1L;
 	
 	private Integer id;
-	private String nome;
 	
+	@NotEmpty(message="Preenchimento obrigatório")
+	@Length(min=5, max=80, message="O tamanho deve ser entre 5 e 80 caracteres")
+	private String nome;
+
 	public CategoriaDTO() {
-		
 	}
 	
 	public CategoriaDTO(Categoria obj) {
@@ -34,6 +39,5 @@ public class CategoriaDTO implements Serializable {
 
 	public void setNome(String nome) {
 		this.nome = nome;
-	}
-	
+	}	
 }
